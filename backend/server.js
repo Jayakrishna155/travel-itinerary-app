0d5a0bfebae24c168e89a040fdf94df6ad5618ee
JAYAKRISHNA <143500077+Jayakrishna155@users.noreply.github.com>
2025-12-28 22:19:40 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: process.env.FRONTEND_URL || 'travel-itinerary-app-frontend.vercel.app',
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
